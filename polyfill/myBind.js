aa1fcf889acd2f2f9ea571406bc8dc6e6e2624cd
Sonu Shahuji <sonushahuji4@gmail.com>
2024-04-09 18:16:15 +0530

    polyfill for bind  function

---
+++ polyfill/myBind.js
@@ -0,0 +1,29 @@
+// {}
+
+const personOne = {
+    name: "Javascript"
+}
+
+const personTwo = {
+    name: "Java"
+}
+
+function personAge (age) {
+    console.log(this.name, " is ", age, " old")
+}
+
+const newFun = personAge.bind(personOne)
+newFun(100)
+
+Function.prototype.myBind = function(obj={}, ...args1){
+    if(typeof this !== 'function'){
+        throw new Error('Not callable')
+    }
+    obj.fn = this
+    return function(...args2){
+        obj.fn(...args1, ...args2)
+    }
+}
+
+const newFunOne = personAge.myBind(personOne)
+newFunOne(100)
