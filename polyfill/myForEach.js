2c4e1128b32d22a8683850c386e08960fa4a6079
Sonu Shahuji <sonushahuji4@gmail.com>
2024-04-09 09:20:01 +0530

    foEach polyfill

---
+++ polyfill/myForEach.js
@@ -0,0 +1,13 @@
+const arr = [1,2,3,4,5]
+
+// Actualy Method
+arr.forEach((item) => console.log(item))
+
+// Created new polyfill method which will mimic as forEach
+Array.prototype.myForEach = function(cb){
+    for(index= 0; index < this.length; index++){
+        cb(this[index])
+    }
+}
+
+arr.myForEach((item) => console.log(item))
